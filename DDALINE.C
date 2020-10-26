d4a49a0702d6e753cb835be2b90caab42cc7a253
khurohan <73482119+khurohan@users.noreply.github.com>
2020-10-26 20:04:20 +0530

    Update DDALINE.C

--- DDALINE.C
+++ DDALINE.C
@@ -3,13 +3,11 @@
 #include<math.h>
 #include<dos.h>
 
-void main()
-{ int gd=DETECT,gm,x,y,x2,y2,dx,dy,i,steps;
-  float xin,yin,x1,y1;
+int graphicdriver=DETECT,graphicmode,x,y,x2,y2,dx,dy,i,steps;
+float xincrement,yincrement,x1,y1;
 
-  clrscr();
-
-  printf("Enter Coordinates of point A(x1,y1) :-\n");
+void getcoordinates()
+{ printf("Enter Coordinates of point A(x1,y1) :-\n");
   printf("\nEnter x1 : ");
   scanf("%f",&x1);
   printf("\nEnter y1 : ");
@@ -20,40 +18,42 @@ void main()
   scanf("%d",&x2);
   printf("\nEnter y2 : ");
   scanf("%d",&y2);
+}
 
-  dx=x2-x1;
+void calculations()
+{ dx=x2-x1;
   dy=y2-y1;
-
-  //printf("\ndx = %d , dy = %d",dx,dy);
-  //getch();
 
   if(abs(dx)>=abs(dy))
   { steps=abs(dx); }
   else
   { steps=abs(dy); }
 
-  //printf("\nsteps = %d",steps);
-  //getch();
-
-  xin=(float)dx/steps;
-  yin=(float)dy/steps;
-
-  //printf("\nxin = %f , yin = %f",xin,yin);
-  //getch();
+  xincrement=(float)dx/steps;
+  yincrement=(float)dy/steps;
 
   x=(int)x1;
   y=(int)y1;
+}
 
-  initgraph(&gd,&gm,"C:\\TURBOC3\\BGI");
-  for(i=0;i<=steps;i++)
+void plot();
+{ for(i=0;i<=steps;i++)
   { putpixel(x,y,WHITE);
     //printf("\nx = %d , y = %d",x,y);
-    x1=x1+xin;
+    x1=x1+xincrement;
     x=(int)x1;
-    y1=y1+yin;
+    y1=y1+yincrement;
     y=(int)y1;
     delay(23);
   }
+}
+
+void main()
+{ clrscr();
+  initgraph(&graphicdriver,&graphicmode,"C:\\TURBOC3\\BGI");
+  getcoordinates();
+  calculations();
+  plot();
   getch();
   closegraph();
 }
